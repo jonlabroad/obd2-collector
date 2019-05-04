40ca34570ae8fbfa93aefd196d0b51c942d6e9c4
Jonathan LaBroad <jon.labroad@gmail.com>
2019-05-04 13:18:41 -0400

    log file name

--- collector/start.py
+++ collector/start.py
@@ -7,7 +7,7 @@ import jdlDate
 import fileCache
 
 try:
-    logging.basicConfig(filename='log.txt', level=logging.INFO)
+    logging.basicConfig(filename='log_' + jdlDate.getCalendarDateTime() + '.txt', level=logging.INFO)
     logging.info("Starting poll...")
     poll.poll()
     logging.info("Polling complete")
